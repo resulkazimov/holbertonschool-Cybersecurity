9a2747f135e51ccef8d0185c34251b280dd97f91
resulkazimov <rsulkazimov29@gmail.com>
2026-05-29 15:49:24 +0000

    Fix task 4 bitwise length

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mask=$(( $1 == 0 ? 0 : 0xffffffff << (32 - $1) )); printf "%d.%d.%d.%d\n" $(( (mask >> 24) & 255 )) $(( (mask >> 16) & 255 )) $(( (mask >> 8) & 255 )) $(( mask & 255 ))
+mask=$(( 0xffffffff ^ ((1 << (32 - $1)) - 1) )); [ $1 -eq 0 ] && mask=0; printf "%d.%d.%d.%d\n" $(( (mask >> 24) & 255 )) $(( (mask >> 16) & 255 )) $(( (mask >> 8) & 255 )) $(( mask & 255 ))
